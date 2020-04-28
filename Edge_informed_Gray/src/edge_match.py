326c5c7325cb0fccfdae0cd5fbe8d9bf9b8c10c5
Phung Huynh <phunghx@gmail.com>
2020-04-28 21:08:44 +0700

    gray

--- Edge_informed_Gray/src/edge_match.py
+++ Edge_informed_Gray/src/edge_match.py
@@ -29,11 +29,12 @@ class EdgeMatch():
         #import pdb;pdb.set_trace()
         self.psnr = PSNR(255.0).to(config.DEVICE)
         self.edgeacc = EdgeAccuracy(config.EDGE_THRESHOLD).to(config.DEVICE)
-
-        self.test_dataset = DatasetOri(config.TEST_FLIST_LR, config.TEST_FLIST_LR, sigma=config.SIGMA, scale=4, hr_size=0, augment=False)
-        self.train_dataset = Dataset('train', sigma=config.SIGMA, scale=config.SCALE, hr_size=config.HR_SIZE, augment=True)
-        self.val_dataset = Dataset('val', sigma=config.SIGMA, scale=config.SCALE, hr_size=config.HR_SIZE, augment=False)
-        self.sample_iterator = self.val_dataset.create_iterator(config.SAMPLE_SIZE)
+        if self.config.MODE==2:
+             self.test_dataset = DatasetOri(config.TEST_FLIST_LR, config.TEST_FLIST_LR, sigma=config.SIGMA, scale=4, hr_size=0, augment=False)
+        else:
+             self.train_dataset = Dataset('train', sigma=config.SIGMA, scale=config.SCALE, hr_size=config.HR_SIZE, augment=True)
+             self.val_dataset = Dataset('val', sigma=config.SIGMA, scale=config.SCALE, hr_size=config.HR_SIZE, augment=False)
+             self.sample_iterator = self.val_dataset.create_iterator(config.SAMPLE_SIZE)
 
         self.samples_path = os.path.join(config.PATH, 'samples')
         self.results_path = os.path.join(config.PATH, 'results')
